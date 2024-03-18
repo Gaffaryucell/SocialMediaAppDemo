ee23913bbdde51f48c86ade6d4470c38d7eae00c
gaffar <gaffaryucell@gmail.com>
2024-03-18 21:41:53 +0300

    demo tamamlandı

--- app/src/main/java/com/androiddevelopers/socialmediaappdemo/view/EntryActivity.kt
+++ app/src/main/java/com/androiddevelopers/socialmediaappdemo/view/EntryActivity.kt
@@ -3,6 +3,7 @@ package com.androiddevelopers.socialmediaappdemo.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.androiddevelopers.socialmediaappdemo.MainActivity
 import com.androiddevelopers.socialmediaappdemo.databinding.ActivityEntryBinding
 
@@ -14,8 +15,16 @@ class EntryActivity : AppCompatActivity() {
         binding = ActivityEntryBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
         binding.btnEnty.setOnClickListener{
+            val mail = binding.etMail.text.toString()
+            val password = binding.etPass.text.toString()
+            if (mail.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "lütfen kullanıcı adınızı ve şifrenizi girin", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             val intent = Intent(this,MainActivity::class.java)
+            finish()
             startActivity(intent)
         }
 
